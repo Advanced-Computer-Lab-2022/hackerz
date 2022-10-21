fdceecb5a8754197c77cd27fc31d3565ca3d8a29
Abdullah Abdelhafez <abdullahabdelhafez01@gmail.com>
2022-10-21 16:09:21 +0200

    minor fix

--- backend/routes/instructor.js
+++ backend/routes/instructor.js
@@ -8,7 +8,7 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/addCourse').post((req, res) => {
-  const title = req.body.username;
+  const title = req.body.title;
   const description = req.body.description;
   const duration = Number(req.body.duration);
   const dateAdded = Date.parse(req.body.date);
