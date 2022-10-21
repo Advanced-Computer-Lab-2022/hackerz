e2c336756559054812fd60d2190b50c0ea30864e
Abdullah Abdelhafez <abdullahabdelhafez01@gmail.com>
2022-10-21 20:00:01 +0200

    added regular expression and projection

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,21 +1,26 @@
 const router = require('express').Router();
 let Course = require('../models/course.model');
 
+const projection = {_id:0, __v: 0, createdAt: 0, updatedAt: 0, dateAdded: 0};
+
 router.route('/').get((req, res) => {
     const searchString = req.query.query;
+    const regExp = new RegExp(searchString,'i');  //case-insensitive regular expression
     if (searchString){
-        Course.find({$text: {$search: searchString}}).limit(10)
+        Course.find().or([{title: {$regex: regExp}},{instructor:{$regex: regExp}},{subject:{$regex: regExp}}]).limit(10)
+        .select(projection)
         .then(courses => res.json(courses))
         .catch(err => res.status(500).json('Error: ' + err));} 
     else {
         Course.find().limit(10)
+        .select(projection)
         .then(courses => res.json(courses))
         .catch(err => res.status(500).json('Error: ' + err));
     }
 });
 
 router.route('/:id').get((req, res) => {
-    Course.findById(req.params.id)
+    Course.findById(req.params.id).select(projection)
       .then(course => res.json(course))
       .catch(err => res.status(500).json('Error: ' + err));
   });
