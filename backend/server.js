6635e25e17212899b2401287bfeade978a9ab941
Abdullah Abdelhafez <abdullahabdelhafez01@gmail.com>
2022-10-23 13:06:46 +0200

    fixed mongo URI bug

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
 app.use(cors());
 app.use(express.json());
