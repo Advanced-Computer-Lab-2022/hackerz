426fc496a3e93faae8148d7bbbd779bce4bdbb00
SalmaSalem81 <salmasalem8152@gmail.com>
2022-10-29 00:40:34 +0200

    front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import {BrowserRouter as Router , Routes, Route,  Link} from "react-router-dom";
+//import Navbar from "./components/navbar.component";
+import axios from 'axios';
+
+import React from 'react';
+import ReactDom from"react-dom";
+import Courses from "./components/Courses"
+import Corp_Courses from "./components/Corp_Courses"
+import My_courses from "./components/My_courses"
+
+
+function App ()  {
+  return(
+    <Router>
+    <nav>
+        <Link to ="/courses"> Courses</Link>
+        <Link to ="/corp"> Corp_user</Link>
+        <Link to ="/instructor"> My_courses</Link>
+    </nav>
+    <Routes>
+      <Route path ="/courses" element ={<Courses />}/>
+      <Route path ="/corp" element ={<Corp_Courses />}/>
+      <Route path ="/instructor" element ={<My_courses />}/>
+    
+    </Routes>
+  </Router>
   );
 }
 
