8d823cb158a0f23afd8b8a390393125fc67ef05a
Abdullah Abdelhafez <abdullahabdelhafez01@gmail.com>
2022-10-29 20:24:16 +0200

    added href to course

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 export default function Course({course}) {
+  const link = "/courses/" + course._id;
   return (
     <div>
-        <a href="#">{course.title}</a> by {course.instructorUsername} 
-        {course.price === 0 ? <span style={{color: "red"}}> (FREE)</span> : <> for ${course.price}</>}
+        <a href={link}>{course.title}</a> by {course.instructorUsername} 
+        { course.price ? (course.price === 0 ?
+         <span style={{color: "red"}}> (FREE)</span> 
+         : <> for ${course.price}</>):<></>}
         <hr></hr>
     </div>
   )
