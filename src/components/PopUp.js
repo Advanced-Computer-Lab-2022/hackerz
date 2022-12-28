6e738c8149dc5411c8240068bf733b693228b78e
Rawan Darwish <rawanmdarwish.01@gmail.com>
2022-12-28 11:21:16 +0200

    popup

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -7,10 +7,11 @@ export default function PopUp(props) {
   return (props.trigger) ? ( 
     <div className='popup'>
       <div className='popup-inner'>
-        <h3>Company's Contract</h3>
+      <h3>Company's Contract</h3>
         <p>The company owns all the rights to the posted videos and materials.</p>
         <p> the company takes 12% of all the revenues of the sold courses.</p>
         <Button onClick={() => props.setTrigger(false)} className="accept-btn w-25 mx-auto mb-3" variant="primary">Close</Button>
+        {/* {props.children} */}
       </div>
     </div>
   ) : "";
